7856ef7900b9927d67d498fa1c9e30ac246061b5
= <lrbabe@gmail.com>
2015-08-25 17:07:37 +0200

    cleanup index.html and init.js

--- src/utils/init.js
+++ src/utils/init.js
@@ -38,34 +38,4 @@ module.exports = function init( opts ) {
 	}).then(function() {
 		return new constructor( opts );
 	});
-
-	// return ( opts.prototypoSource ?
-	// 	Promise.resolve( opts.prototypoSource ) :
-	// 	// fetch the resource from URL
-	// 	fetch( opts.prototypoUrl )
-	//
-	// ).then(function( result ) {
-	// 	return typeof result === 'string' || result.text();
-	//
-	// }).then(function( result ) {
-	// 	if ( result !== true ) {
-	// 		opts.prototypoSource = result;
-	// 	}
-	//
-	// 	// the worker can be loaded from a file by specifying its url (dev
-	// 	// environment), or by building it as a blob, from a require'd file.
-	// 	if ( opts.workerUrl ) {
-	// 		// The search fragment of workerUrl must include prototypo.js URL
-	// 		opts.workerUrl +=
-	// 			'?bundleurl=' + encodeURIComponent( opts.prototypoUrl );
-	//
-	// 	} else {
-	//
-	// 		// opts.workerUrl +=
-	// 		// 	'?bundleurl=' + encodeURIComponent( opts.prototypoUrl );
-	// 		// console.log(opts.workerUrl);
-	// 	}
-
-	// }).then(function() {
-
 };
