14ecff657c09544873645a62b75b58126896ae8e
FranzPoize <francois.poizat@gmail.com>
2016-04-01 18:09:31 +0200

    partition zoom and avoid to much dezoom

--- src/utils/mouseHandlers.js
+++ src/utils/mouseHandlers.js
@@ -1,27 +1,28 @@
 var paper = require('prototypo.js').paper;
 
 function wheelHandler( event ) {
-	var bcr = this.canvas.getBoundingClientRect(),
-		currPos = new paper.Point(
-			event.clientX - bcr.left,
-			event.clientY - bcr.top
-		),
-		viewPos = this.view.viewToProject( currPos ),
-		// normalize the deltaY value. Expected values are ~40 pixels or 3 lines
-		factor = 1 + ( this.opts.zoomFactor *
+	// normalize the deltaY value. Expected values are ~40 pixels or 3 lines
+	var factor = 1 + ( this.opts.zoomFactor *
 			( Math.abs( event.deltaY / event.deltaMode ? 3 : 40 * 20 ) ) ),
 		newZoom =
 			event.deltaY < 0 ?
 				this.view.zoom * factor :
 				event.deltaY > 0 ?
 					this.view.zoom / factor :
-					this.view.zoom,
-		beta = this.view.zoom / newZoom,
-		difference = viewPos.subtract( this.view.center ),
-		newCenter = viewPos.subtract( difference.multiply(beta) );
+					this.view.zoom;
+		
 
-	this.zoom = newZoom;
-	this.view.center = newCenter;
+	if ( newZoom > 0.07 ) {
+		var mousePosition = new paper.Point( event.offsetX, event.offsetY );
+		var viewPosition = this.view.viewToProject( mousePosition );
+		var pc = viewPosition.subtract( this.view.center );
+		var newPosition = viewPosition.subtract(
+			pc.multiply( this.view.zoom / newZoom )
+		).subtract( this.view.center );
+
+		this.zoom = newZoom;
+		this.view.center = this.view.center.add(newPosition);
+	}
 
 	event.preventDefault();
 }
